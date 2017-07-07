f00e9c46f9190a20a5a2f00a688abf044af505b9
Pavan Puranik <puranik01@gmail.com>
2017-07-07 23:01:21 +0530

    Jasper Reports Engine
    Initial Skeletal Commit

---
+++ jasper-reports-file/src/main/java/org/jasper/reports/JasperReportsFileApplication.java
@@ -0,0 +1,12 @@
+package org.jasper.reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperReportsFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperReportsFileApplication.class, args);
+	}
+}
